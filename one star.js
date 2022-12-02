f91709eb39b96bebe4cbbcacb1d6a647da85795f
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2022-12-02 13:38:39 +0300

    Update one star.js

--- one star.js
+++ one star.js
@@ -9,35 +9,22 @@ class COAXManag {
     }
 
     get position() {
-        return this._position;
+        this._position;
     }
     set position(value) {
-        if(value === 'ITManag') {
-            return this._position = value;
-        } else if(value === 'SysAdmin') {
-            return this._position = value;
-        } else if(value === 'JSDeveloper') {
-            return this._position = value;
-        } else if(value === 'DevOps') {
-            return this._position = value;
-        } 
-
-        this._position = undefined;
+        if (['ITManag', 'SysAdmin', 'JSDeveloper', 'DevOps'].includes(value)) this._position = value
+        else this._position = null;
     }
 
     get yearOfEmployment() {
-        return this._position;
+        this._position;
     }
     set yearOfEmployment(value) {
-        if(value > 2022) {
-            return this._yearOfEmployment = undefined;
-        } else if (Date()) {
-            return this._yearOfEmployment = value;
-        }
-        
+        if (value > 2022) this._yearOfEmployment = null;
+        else this._yearOfEmployment = value;       
     }
 }
 
-const coaxmanag = new COAXManag('Roman', 'Roketskyi', 380665213423, 'DevOps', 'Software engineering', 2023); 
+const coaxmanag = new COAXManag('Roman', 'Roketskyi', 380665213423, 'DevOps', 'Software engineering', 2022); 
 
 console.log(coaxmanag)
